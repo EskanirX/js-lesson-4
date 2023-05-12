100b8a38c488c44a5270d4677d01e6762c1c97f1
EskanirX <125078699+EskanirX@users.noreply.github.com>
2023-05-13 01:43:16 +0400

    end

---
+++ script.js
@@ -0,0 +1,123 @@
+'use strict'
+
+// let numbers = [5, 25, 89, 120, 36]
+
+// numbers.push('javascript','phyton')
+// numbers.unshift('html','css')
+// // console.log(numbers.length);
+// numbers.pop()
+// numbers.shift()
+// // console.log(numbers);
+
+
+// let fruits = ['ფორთოხალი','ბანანი','მსხალი']
+// // console.log(fruits.length);
+// fruits.push('ვაშლი','ანანასი')
+// fruits.unshift('საზამთრო')
+// // console.log(fruits.length);
+// fruits.splice(2,0,'მანგო')
+// // console.log(fruits);
+// fruits.pop()
+// fruits.shift()
+// // console.log(fruits);
+// console.log(fruits.length);
+
+
+// let array =[1, 2, 3, 4, 5]
+// array.splice(3,0,'a','b','c')
+// console.log(array);
+
+
+
+// let arr = [1, 2, 3, 4, 5];
+// let sum = 0
+// arr.forEach(function(x){
+//     sum += x;
+
+// })
+// console.log(sum);
+
+
+
+// let number = 100
+// for (let x = 100; x > 0; x --){
+//     console.log(x);
+// }
+
+
+
+// let numbers = function(num1, num2){
+//     if (num1 > num2){
+//         console.log("num1 is larger");
+//     } else if (num2 > num1){
+//         console.log("num2 is larger");
+//     } else {console.log('error');}
+// }
+// numbers(100,200)
+// console.log(numbers);
+
+
+
+
+// let array = [12, 25, 3, 6, 8, 14, 7, 23];
+
+
+// let array2 = array.map(function(x) {
+//     return x / 3
+// })
+// console.log(array2);
+
+
+
+
+
+// let languages = ['html', 'css', 'javascript', 'python', 'php'];
+
+// let languages2 = languages.filter(function(x) {
+//     return(x.length > 3)
+// });
+// console.log(languages2);
+
+
+
+
+// let words = ['Madrid', 'Rome', 'Milan', 'Berlin'];
+
+
+// let words2 = words.filter(function(c) {
+//     if (c.includes('m') || c.includes ('M')) {
+//         return c
+//     }return false
+// })
+// console.log(words2);
+
+
+
+
+
+// let arr1 = [1, 2];
+// let arr2 = [3, 4];
+// let arr3 = [5, 6];
+
+// let arrAll = arr1.concat(arr2 , arr3)
+// console.log(arrAll);
+
+
+
+
+
+// let arr = [-1, -2, -3, 4];
+
+// let arr1 = arr.some(function (x) {
+//     if(x > 0){
+//         return true;
+//     }
+//     return false
+// })
+// console.log(arr1);
+
+
+
+// let array=[2,15,10,24]
+// array.splice(2,1)
+// console.log(array);
